34cbbf70c116679204c2ad5ee6fdf0c2c87b802a
husen <husenmalik7@gmail.com>
2024-10-22 10:01:07 +0700

    update: change credential db in config

--- src/config/db.js
+++ src/config/db.js
@@ -1,11 +1,11 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-  user: "qafdqoefuzmbhi",
-  host: "ec2-3-234-22-132.compute-1.amazonaws.com",
-  database: "d2t80k7j6r1jic",
-  password: "d18b56a75613b61f08e01347bd6723d0c91b4cb593b50ed2c312572a1d14593c",
-  port: 5432,
+  user: "postgres",
+  host: "autorack.proxy.rlwy.net",
+  database: "railway",
+  password: "GYBAQAuWgzpqrgdOfJfWJsDhuTXGHOgp",
+  port: 41194,
   // ssl: true,
 
   ssl: {
